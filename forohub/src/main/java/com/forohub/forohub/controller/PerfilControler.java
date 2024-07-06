b0f4b82155bec2387c52ea66612eb7e86f51d9e9
Kevin Castillo <kevin.castilloxc@gmail.com>
2024-07-06 06:19:23 -0600

    Creando tabla -perfiles- y añadiendo migración

---
+++ forohub/src/main/java/com/forohub/forohub/controller/PerfilControler.java
@@ -0,0 +1,11 @@
+package com.forohub.forohub.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/perfil")
+public class PerfilControler {
+
+}
---
+++ forohub/src/main/resources/db.migration/V1__create-table-perfiles.sql
@@ -0,0 +1,8 @@
+CREATE TABLE 'perfiles' (
+
+  'id' INT NOT NULL AUTO_INCREMENT,
+  'nombre' VARCHAR(100) NOT NULL,
+
+  PRIMARY KEY ('id')
+
+  );
